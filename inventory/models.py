9cd934ec20d9f329c6564c1a7f8ab1747fbe8da7
Meraj <merajsafari3@gmail.com>
2023-10-02 15:00:14 +0330

    created inventory base model

--- inventory/models.py
+++ inventory/models.py
@@ -1,33 +1,24 @@
 from django.db import models
 from django_jalali.db import models as jmodels
 
-class Invoice(models.Model):
+class Inventory(models.Model):
 
     DELETED_CHOICES = (
         ('Y', 'Yes'),
         ('N', 'No'),
     )
 
-    STATUS_CHOICES = (
-        ('c', 'پرداخت شده با کارت خوان'),
-        ('n', 'پرداخت نقدی'),
-        ('k', 'پرداخت کارت به کارت'),
-        ('u', 'پرداخت نشده'),
-    )    
-
     id = models.AutoField(primary_key=True)
-    code = models.CharField(blank=True, null=True)
+    name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100, unique=True, blank=True, null=True)
+    decription = models.TextField(blank=True, null=True)
     created_at = jmodels.jDateTimeField(blank=True, null=True)
     updated_at = jmodels.jDateTimeField(blank=True, null=True)
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='u')
-    date = jmodels.jDateField(blank=True, null=True)
-    decription = models.TextField(blank=True, null=True)
     is_deleted = models.CharField(max_length=1, choices=DELETED_CHOICES, blank=True, null=True, default="N")
 
 
     def __str__(self):
-        return f'{self.code} {self.date}'
+        return f'{self.name}'
 
 
     def save(self, *args, **kwargs):
@@ -43,4 +34,4 @@ class Invoice(models.Model):
         self.slug = slugify('{}'.format(self.slug))
         self.updated_at = timezone.localtime(timezone.now())
 
-        super(Invoice, self).save(*args, **kwargs)   
+        super(Inventory, self).save(*args, **kwargs)
