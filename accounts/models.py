f082052c6f0ea94fc392804f3f6b5b4e2af7e11b
Meraj <merajsafari3@gmail.com>
2023-10-11 14:23:49 +0330

    created customer and mechanic models for invoice app

--- accounts/models.py
+++ accounts/models.py
@@ -9,10 +9,10 @@ class CustomUser(AbstractUser):
         ('F', 'شرکت')
     )
 
-    type = models.CharField(max_length=1, choices=USER_TYPES, blank=True, null=True, default="C")  
+    type = models.CharField(max_length=1, choices=USER_TYPES, blank=True, null=True, default="C") 
     phone = models.IntegerField(null=True, blank=True)
     address = models.TextField(max_length=500, null=True, blank=True)
 
     def __str__(self):
         return f'{self.username}({self.get_type_display()})' #this shit get 2 hours of my time :)
-        
+       
--- invoice/models.py
+++ invoice/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django_jalali.db import models as jmodels
 from inventory.models import Item
-from accounts.models import CustomUser
+from django.utils import timezone
+from django.template.defaultfilters import slugify
+from uuid import uuid4
+# from accounts.models import CustomUser
 class Invoice(models.Model):
-
     DELETED_CHOICES = (
         ('Y', 'Yes'),
         ('N', 'No'),
@@ -24,7 +26,7 @@ class Invoice(models.Model):
     status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='u')
     date = jmodels.jDateField(blank=True, null=True)
     items = models.ManyToManyField(Item)
-    user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
+    # user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
     decription = models.TextField(blank=True, null=True)
     is_deleted = models.CharField(max_length=1, choices=DELETED_CHOICES, blank=True, null=True, default="N")
 
@@ -47,3 +49,68 @@ class Invoice(models.Model):
         self.updated_at = timezone.localtime(timezone.now())
 
         super(Invoice, self).save(*args, **kwargs)   
+class Customer(models.Model):
+    DELETED_CHOICES = (
+        ('Y', 'Yes'),
+        ('N', 'No'),
+    )
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(blank=True, null=True, max_length=150)
+    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
+    phone = models.CharField(blank=True, null=True, max_length=50)
+    address = models.TextField(blank=True, null=True)
+    created_at = jmodels.jDateTimeField(blank=True, null=True)
+    updated_at = jmodels.jDateTimeField(blank=True, null=True)
+    is_deleted = models.CharField(max_length=1, choices=DELETED_CHOICES, blank=True, null=True, default="N")
+    
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs): 
+        if not self.is_deleted:
+            self.is_deleted = 'N' 
+
+        if not self.id:
+            self.created_at = timezone.localtime(timezone.now())
+              
+        if not self.slug:
+            self.slug = str(uuid4()).split('-')[4]
+
+        self.slug = slugify('{}'.format(self.slug))
+        self.updated_at = timezone.localtime(timezone.now())
+
+        super(Customer, self).save(*args, **kwargs)
+
+class Mechanic(models.Model):
+    DELETED_CHOICES = (
+        ('Y', 'Yes'),
+        ('N', 'No'),
+    )
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(blank=True, null=True, max_length=150)
+    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
+    phone = models.CharField(blank=True, null=True, max_length=50)
+    address = models.TextField(blank=True, null=True)
+    created_at = jmodels.jDateTimeField(blank=True, null=True)
+    updated_at = jmodels.jDateTimeField(blank=True, null=True)
+    is_deleted = models.CharField(max_length=1, choices=DELETED_CHOICES, blank=True, null=True, default="N")
+    
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs): 
+        if not self.is_deleted:
+            self.is_deleted = 'N' 
+
+        if not self.id:
+            self.created_at = timezone.localtime(timezone.now())
+              
+        if not self.slug:
+            self.slug = str(uuid4()).split('-')[4]
+
+        self.slug = slugify('{}'.format(self.slug))
+        self.updated_at = timezone.localtime(timezone.now())
+
+        super(Mechanic, self).save(*args, **kwargs)
