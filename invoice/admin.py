806bdb519222012f582df575cada19bb4353ba78
Meraj <merajsafari3@gmail.com>
2023-10-12 22:22:09 +0330

    plan changed! model structures are changed

--- invoice/admin.py
+++ invoice/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from .models import Invoice, Customer, Mechanic
+from .models import Invoice
 
 admin.site.register(Invoice)
-admin.site.register(Customer)
-admin.site.register(Mechanic)
--- invoice/models.py
+++ invoice/models.py
@@ -1,11 +1,83 @@
 from django.db import models
 from django_jalali.db import models as jmodels
-from inventory.models import Item
 from django.utils import timezone
 from django.template.defaultfilters import slugify
 from uuid import uuid4
-# from accounts.models import CustomUser
+
+class Category(models.Model):
+    
+    DELETED_CHOICES = (
+        ('Y', 'Yes'),
+        ('N', 'No'),
+    )
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(blank=True, null=True, max_length=150)
+    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
+    created_at = jmodels.jDateTimeField(blank=True, null=True)
+    updated_at = jmodels.jDateTimeField(blank=True, null=True)    
+    is_deleted = models.CharField(max_length=1, choices=DELETED_CHOICES, blank=True, null=True, default="P")
+
+    def __str__(self):
+        return f"{self.name}"
+               
+    def save(self, *args, **kwargs):
+        if not self.is_deleted:
+            self.is_deleted = 'N'
+        
+        if not self.id:
+            self.created_at = timezone.localtime(timezone.now())
+        
+        if not self.slug:
+            self.slug = str(uuid4()).split('-')[4]
+
+        self.slug = slugify('{}'.format(self.slug))
+        self.updated_at = timezone.localtime(timezone.now())
+
+        super(Category, self).save(*args, **kwargs)
+
+class Item(models.Model):
+
+    DELETED_CHOICES = (
+        ('Y', 'Yes'),
+        ('N', 'No'),
+    )
+
+    ITEM_TYPES = (
+        ('S', 'Service'),
+        ('p', 'Product'),
+    )
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(blank=True, null=True, max_length=150)
+    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
+    type = models.CharField(max_length=1, choices=ITEM_TYPES, blank=True, null=True, default="P")
+    price = models.IntegerField(blank=True, null=True)
+    qty = models.IntegerField(blank=True, null=True)
+    category = models.ManyToManyField(Category)
+    created_at = jmodels.jDateTimeField(blank=True, null=True)
+    updated_at = jmodels.jDateTimeField(blank=True, null=True)    
+    is_deleted = models.CharField(max_length=1, choices=DELETED_CHOICES, blank=True, null=True, default="N")
+
+    def __str__(self):
+        return f"{self.name} / ({self.price:,d})"
+        
+    def save(self, *args, **kwargs):
+        if not self.is_deleted:
+            self.is_deleted = 'N'
+        
+        if not self.id:
+            self.created_at = timezone.localtime(timezone.now())
+        
+        if not self.slug:
+            self.slug = str(uuid4()).split('-')[4]
+
+        self.slug = slugify('{}'.format(self.slug))
+        self.updated_at = timezone.localtime(timezone.now())
+
+        super(Item, self).save(*args, **kwargs)
 class Customer(models.Model):
+
     DELETED_CHOICES = (
         ('Y', 'Yes'),
         ('N', 'No'),
@@ -38,43 +110,15 @@ class Customer(models.Model):
 
         super(Customer, self).save(*args, **kwargs)
 
-class Mechanic(models.Model):
-    DELETED_CHOICES = (
-        ('Y', 'Yes'),
-        ('N', 'No'),
-    )
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(blank=True, null=True, max_length=150)
-    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
-    phone = models.CharField(blank=True, null=True, max_length=50)
-    address = models.TextField(blank=True, null=True)
-    created_at = jmodels.jDateTimeField(blank=True, null=True)
-    updated_at = jmodels.jDateTimeField(blank=True, null=True)
-    is_deleted = models.CharField(max_length=1, choices=DELETED_CHOICES, blank=True, null=True, default="N")
-    
-
-    def __str__(self):
-        return self.name
-
-    def save(self, *args, **kwargs): 
-        if not self.is_deleted:
-            self.is_deleted = 'N' 
-
-        if not self.id:
-            self.created_at = timezone.localtime(timezone.now())
-              
-        if not self.slug:
-            self.slug = str(uuid4()).split('-')[4]
-
-        self.slug = slugify('{}'.format(self.slug))
-        self.updated_at = timezone.localtime(timezone.now())
-
-        super(Mechanic, self).save(*args, **kwargs)
-
 class Invoice(models.Model):
     DELETED_CHOICES = (
         ('Y', 'Yes'),
         ('N', 'No'),
+    )
+
+    INVOICE_TYPE = (
+        ('W', 'فاکتور خدمات'),
+        ('M', 'فاکتور فروش'),
     )
 
     STATUS_CHOICES = (
@@ -90,14 +134,13 @@ class Invoice(models.Model):
     created_at = jmodels.jDateTimeField(blank=True, null=True)
     updated_at = jmodels.jDateTimeField(blank=True, null=True)
     status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='u')
+    type = models.CharField(max_length=1, choices=INVOICE_TYPE, blank=True, null=True)
     date = jmodels.jDateField(blank=True, null=True)
     items = models.ManyToManyField(Item)
 
     #related fields
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
-    mechanic = models.ForeignKey(Mechanic, on_delete=models.SET_NULL, blank=True, null=True)
 
-    # user = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True, blank=True)
     decription = models.TextField(blank=True, null=True)
     is_deleted = models.CharField(max_length=1, choices=DELETED_CHOICES, blank=True, null=True, default="N")
 
